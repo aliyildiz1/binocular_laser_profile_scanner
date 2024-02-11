bfec10b7454d4ccbb7e43d198178cbc3137fa88b
aliyildiz1 <aliyildiz2588@gmail.com>
2024-02-11 16:12:05 +0300

    modify test scripts

--- scripts/test/laser_triangulation_test.py
+++ scripts/test/laser_triangulation_test.py
@@ -2,6 +2,7 @@
 
 import math
 
+# pixel coordinates
 x = 300
 y = 300
 
@@ -19,19 +20,24 @@ l_c = 80 # laser-camera distance
 l_o = 70 # laser-origin distance
 beta = math.atan(l_c/l_o) # Laser - Origin - Camera angle
 v_theta = math.atan(c_y/fc) # vertical theta
-h_theta = math.atan(c_x/fc)
+h_theta = math.atan(c_x/fc) # horizontal theta
 theta = math.atan(math.sqrt(c_x**2 + c_y**2)/fc) # theta
 v_fov = 2 * math.atan(h / (2 * fc)) # vertical FOV
 
-f_o = math.sqrt(l_c**2 + l_o**2) - fc
+f_o = math.sqrt(l_c**2 + l_o**2) - fc # F-origin distance
 
-pz = f_o * math.sin(v_theta)/math.sin(beta + v_theta)
+pz = f_o * math.sin(v_theta)/math.sin(beta + v_theta) # z axis coordinate
 
-o_c = f_o*math.sin(beta)
+p_o_f = f_o*math.sin(beta) # projection of F-origin distance
 
-py = (c_x*o_c*math.sin(beta))/(fc*math.sin(beta)+c_y*math.cos(beta))
+py = (c_x*p_o_f*math.sin(beta))/(fc*math.sin(beta)+c_y*math.cos(beta)) # y axis coordinate
+
+# translation of y-z coordinates from origin to laser coordinate system
+# unit changed mm to m
+py = (-1 * py) / 1000
+pz = ((-1 * pz) - l_o) / 1000
 
 print("x: ", x)
 print("y: ", y)
-print("Pz: ", -pz)
-print("Py: ", -py)
+print("Py: ", py)
+print("Pz: ", pz)
--- scripts/test/thinning_test.py
+++ scripts/test/thinning_test.py
@@ -3,77 +3,13 @@
 import numpy as np
 import math
 import cv2
-"""
-def nothing(x):
-    pass
 
-cv2.namedWindow("Trackbars")
-cv2.createTrackbar("L - B", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("L - G", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("L - R", "Trackbars", 220, 255, nothing)
-cv2.createTrackbar("U - B", "Trackbars", 50, 255, nothing)
-cv2.createTrackbar("U - G", "Trackbars", 50, 255, nothing)
-cv2.createTrackbar("U - R", "Trackbars", 255, 255, nothing)
-
-while True:
-    img = cv2.imread("src/binocular_laser_profile_scanner/scripts/right_cam_image.png")
-
-    # ----------------------------------------------------------
-    # COLOR FILTERING
-    # ----------------------------------------------------------
-
-    cv2.namedWindow("Trackbars")
-    l_b = cv2.getTrackbarPos("L - B", "Trackbars")
-    l_g = cv2.getTrackbarPos("L - G", "Trackbars")
-    l_r = cv2.getTrackbarPos("L - R", "Trackbars")
-    u_b = cv2.getTrackbarPos("U - B", "Trackbars")
-    u_g = cv2.getTrackbarPos("U - G", "Trackbars")
-    u_r = cv2.getTrackbarPos("U - R", "Trackbars")
-
-    lower_red = np.array([l_b, l_g, l_r])
-    upper_red = np.array([u_b, u_g, u_r])
-
-    mask = cv2.inRange(img, lower_red, upper_red)
-
-    result = cv2.bitwise_and(img, img, mask=mask)
-
-    # ----------------------------------------------------------
-    # THINNING
-    # ----------------------------------------------------------
-    height, width, channels = img.shape
-
-    # non zero pixel coordinates
-    pts = cv2.findNonZero(mask)
-
-    if cv2.findNonZero(img) is not None:
-        # Connected component analysis
-        nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity=4)
-
-
-
-    cv2.imshow("img", img)
-    # cv2.imshow("hsv", hsv)
-    cv2.imshow("mask", mask)
-    cv2.imshow("result", result)
-    
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
-
-cv2.destroyAllWindows()
-"""
-
-img = cv2.imread("src/binocular_laser_profile_scanner/scripts/right_cam_image.png")
+img = cv2.imread("src/binocular_laser_profile_scanner/scripts/test/right_cam_image.png")
 mask = cv2.inRange(img, np.array([0, 0, 220]), np.array([50, 50, 255]) )
 
 # non zero pixel coordinates
 pts = cv2.findNonZero(mask)
-"""
-İnceltme algoritması
-1) Eğer görüntüde maskelenmiş bileşenler varsa işleme devam et
-2) görüntüye bağlantılı bileşen analizi uygula
-3) her bir objeyi incelt
-"""
+
 output_img = np.zeros(mask.shape, dtype="uint8")
 
 if pts is not None:
